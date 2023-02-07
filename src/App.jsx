114db4482945ec79e11ecd99de690dd9ee6f64b9
D1ALP3MU <diegoalejandroperezm@gmail.com>
2023-02-06 19:50:31 -0500

    Se consume la api de pokemon mediante el fetch y se traen los nombres y las imágines de los pokemones para después pintarlas en las tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,46 @@ import './sass/App.scss'
 import { TiArrowLeftOutline } from "react-icons/ti";
 import { TiArrowRightOutline } from "react-icons/ti";
 // Hooks:
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
 
     const [pokemonId, setPokemonId] = useState(1);
+    const [pokemonEvolutions, setPokemonEvolutions] = useState([]); 
+
+    useEffect(()=> {
+        getEvolutions(pokemonId);
+    }, [pokemonId])
+
+    async function getEvolutions(id) {
+        const response = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`);
+        const data = await response.json();
+
+        let pokemonEvolutionsArray = [];
+
+        let pokemonLv1 = data.chain.species.name;
+        let pokemonLv1Img = await getPokemonImages(pokemonLv1);
+        pokemonEvolutionsArray.push([pokemonLv1, pokemonLv1Img]);
+
+        if(data.chain.evolves_to.length !== 0) {
+            let pokemonLv2 = data.chain.evolves_to[0].species.name;
+            let pokemonLv2Img = await getPokemonImages(pokemonLv2);
+            pokemonEvolutionsArray.push([pokemonLv2, pokemonLv2Img]);
+
+            if(data.chain.evolves_to[0].evolves_to.length !== 0) {
+                let pokemonLv3 = data.chain.evolves_to[0].evolves_to[0].species.name;
+                let pokemonLv3Img = await getPokemonImages(pokemonLv3);
+                pokemonEvolutionsArray.push([pokemonLv3, pokemonLv3Img]);
+                setPokemonEvolutions(pokemonEvolutionsArray);
+            }
+        }
+    }
+
+    async function getPokemonImages(name){
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+        const data = await response.json();
+        return data.sprites.other['official-artwork'].front_default;
+    }
 
     function prevClick(){
         (pokemonId === 1)?
@@ -27,14 +62,15 @@ const App = () => {
         <>
             {/* Tarjetas */}
             <div className="card-container">
-                <Card />
+                {pokemonEvolutions.map(pokemon => 
+                    <Card name={pokemon} />)}
             </div>
             <div className="buttons-container">
                 <Button 
                     icon={<TiArrowLeftOutline />} 
                     handleClick={prevClick}
                 />
-                {pokemonId}
+                {/* {pokemonName} */}
                 <Button 
                 icon={<TiArrowRightOutline />} 
                 handleClick={nextClick} />
